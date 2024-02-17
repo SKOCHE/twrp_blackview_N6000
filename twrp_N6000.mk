9927783e8df8d30426c50eb3749fa884d40d21dd
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2024-02-17 15:59:53 +0700

    Add files via upload

--- twrp_N6000.mk
+++ twrp_N6000.mk
@@ -1,6 +1,17 @@
-#
 # Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 The Team Win Recovery Project
+# Copyright (C) 2023 TeamWin Recovery Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 # Configure base.mk
@@ -11,9 +22,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Configure launch_with_vendor_ramdisk.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
-
-# Configure emulated_storage.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Configure twrp config common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -29,3 +37,7 @@ PRODUCT_MODEL := N6000
 PRODUCT_MANUFACTURER := Blackview
 
 PRODUCT_GMS_CLIENTID_BASE := android-blackview
+
+#ro.build.description=hal_mgvi_64_armv82-user 12 SP1A.210812.016 1689855690 release-keys
+
+#fingerprint=Blackview/N6000_RU/N6000:12/SP1A.210812.016/1689855690:user/release-keys
